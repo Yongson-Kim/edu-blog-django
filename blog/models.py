71593b06d72792bc08008c3dd05184676d27cee0
김용선 <yongson.kim@vntgcorp.com>
2021-06-20 16:00:42 +0900

    post 모델 추가

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+
+    created = models.DateTimeField()
+    # author = models.ForeignKey(User, on_delete=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+
